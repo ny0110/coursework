df3bba5752bbc8c88b6c51ed92477f3be3497968
polina <timoshina.poly@yandex.ru>
2023-05-10 13:59:47 +0300

    Add invalid data handlers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <map>
 #include <string>
-#include "headers/Cookbook.h"
 #include "headers/Recipe.h"
+
+class Cookbook {
+public:
+    void addRecipe(const Recipe& recipe);
+    void removeRecipe(const std::string& recipeName);
+    [[nodiscard]] const Recipe* findRecipe(const std::string& recipeName) const;
+    void printAllRecipeNames() const;
+
+private:
+    std::map<std::string, Recipe> recipes;
+};
+
+bool isValidInput(const std::string& input) {
+    // Add any additional validation checks you require
+    return !input.empty();
+}
 
 int main() {
     Cookbook cookbook;
@@ -11,13 +27,23 @@ int main() {
         std::cout << "Enter command (add, remove, find, list, quit): ";
         std::getline(std::cin, command);
 
-
         if (command == "add") {
             std::string name, instructions;
             std::cout << "Enter recipe name: ";
             std::getline(std::cin, name);
+
+            if (!isValidInput(name)) {
+                std::cout << "Invalid recipe name." << std::endl;
+                continue;
+            }
+
             std::cout << "Enter instructions: ";
             std::getline(std::cin, instructions);
+
+            if (!isValidInput(instructions)) {
+                std::cout << "Invalid instructions." << std::endl;
+                continue;
+            }
 
             cookbook.addRecipe(Recipe(name, instructions));
             std::cout << "Recipe added!" << std::endl;
@@ -25,6 +51,11 @@ int main() {
             std::string name;
             std::cout << "Enter recipe name: ";
             std::getline(std::cin, name);
+
+            if (!isValidInput(name)) {
+                std::cout << "Invalid recipe name." << std::endl;
+                continue;
+            }
 
             cookbook.removeRecipe(name);
             std::cout << "Recipe removed!" << std::endl;
@@ -36,7 +67,12 @@ int main() {
             std::cout << "Enter recipe name: ";
             std::getline(std::cin, name);
 
-            const Recipe *recipe = cookbook.findRecipe(name);
+            if (!isValidInput(name)) {
+                std::cout << "Invalid recipe name." << std::endl;
+                continue;
+            }
+
+            const Recipe* recipe = cookbook.findRecipe(name);
             if (recipe) {
                 std::cout << "Recipe found!" << std::endl;
                 std::cout << "Name: " << recipe->getName() << std::endl;
@@ -44,12 +80,12 @@ int main() {
             } else {
                 std::cout << "Recipe not found." << std::endl;
             }
-
         } else if (command == "quit") {
             break;
         } else {
             std::cout << "Invalid command." << std::endl;
         }
     }
+
     return 0;
 }
